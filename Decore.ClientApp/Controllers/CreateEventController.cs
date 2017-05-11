c1fb4a5ca2960e387a19d78aa481c1748152a3d7
Rasmus Josefsson <josefssonrasmus@hotmail.se>
2017-05-11 14:06:01 +0200

    Folder renames

---
+++ Decore.ClientApp/Controllers/CreateEventController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Decore.ClientApp.EventServiceReference;
+using Decore.ClientApp.ViewModels;
+using Decore.Models;
+
+namespace Decore.ClientApp.Controllers
+{
+
+    public class CreateEventController : Controller
+    {
+
+        private readonly EventServiceClient _eventWCFclient = new EventServiceClient();
+
+
+        public ActionResult Index()
+        {
+            var eventsTypes = new Dictionary<int, string>
+            {
+               {0, "Sittning!"},
+               {1, "Leka"},
+               {2, "Dansa"},
+            };
+
+            var viewModel = new EventViewModel
+            {
+                EventTypes = eventsTypes
+            };
+
+
+            return View(viewModel);
+        }
+
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult CreateEvent(EventViewModel viewModel)
+        {
+
+
+            var eventObject = new Event
+            {
+                Title = viewModel.Title,
+                Description = viewModel.Description,
+                StartDate = DateTime.Now, // ADD TO FORM
+                EndDate = DateTime.Now, // ADD TO FORM
+                SaleStop = DateTime.Now, // ADD TO FORM
+                BasePrice = viewModel.BasePrice,
+                EventType = "Sittning", // ADD TO FORM
+                ZipCode = viewModel.ZipCode,
+                Adress = viewModel.Adress,
+                ImageURL = viewModel.ImageURL,
+                EventOwner = 1, // ADD TO FORM (NEED TO GET USER FIRST)
+                CreatedBy = 1, // ADD TO FORM (NEED TO GET USER FIRST)
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now // IS NOT NEEDED TO SHOW 
+            };
+            _eventWCFclient.CreateEvent(eventObject);
+
+
+            return RedirectToAction("Index", "EventList");
+        }
+
+
+    }
+}
---
+++ Decore.ClientApp/ViewModels/BeverageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Decore.Models;
+
+namespace Decore.ClientApp.ViewModels
+{
+    public class BeverageViewModel
+    {
+        public ICollection<Beverage> Beverages { get; set; }
+        public Beverage Beverage { get; set; }
+        public ICollection<Measure> Measures { get; set; }
+    }
+}
