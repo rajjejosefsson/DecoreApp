e9e8f84a15bb2324fdfb7f9645d9ec17d98f42f2
Rasmus Josefsson <josefssonrasmus@hotmail.se>
2017-05-13 00:44:23 +0200

    change button design padding

--- Decore.ClientApp/App_Start/BundleConfig.cs
+++ Decore.ClientApp/App_Start/BundleConfig.cs
@@ -9,15 +9,12 @@ namespace DecoreApp
         public static void RegisterBundles(BundleCollection bundles)
         {
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-               // "~/Content/bootstrap.css",
-                "~/Content/Site.css"));
-
-
+        
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                      "~/Scripts/jquery-{version}.js",
                      "~/Scripts/bootstrap.js",
                      "~/Scripts/respond.js"));
+
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -27,9 +24,9 @@ namespace DecoreApp
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/content/css").Include(
+                "~/Content/site.css"));
 
         }
     }
