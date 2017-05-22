f39c053a5189f11a7c47e0d3a586c5e281bfc468
matsson_755 <matsson_755@hotmail.com>
2017-05-22 14:19:47 +0200

    alco service

---
+++ Decore.Data/Repositories/BeverageRepository.cs
@@ -0,0 +1,22 @@
+﻿using Decore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decore.Data.Repositories
+{
+    class BeverageRepository
+    {
+        public ICollection<Beverage> GetBeverages()
+        {
+            using (DecoreDbContext context = new DecoreDbContext())
+            {
+                return context.Beverages.AsNoTracking().ToList();
+            }
+        }
+
+
+    }
+}
