8208ec09f7ffff404725adef1ad9c7240fc51482
Vidar asp <vidar_r60@hotmail.com>
2017-05-30 17:44:23 +0200

    //log

--- Decore.ClientApp/Properties/AssemblyInfo.cs
+++ Decore.ClientApp/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: log4net.Config.XmlConfigurator(ConfigFile = "Web.config", Watch = true)] // For log4net
+//[assembly: log4net.Config.XmlConfigurator(ConfigFile = "Web.config", Watch = true)] // For log4net
