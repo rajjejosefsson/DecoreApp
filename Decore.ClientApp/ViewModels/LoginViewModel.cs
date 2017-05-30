c283d9cf26733159e4aff2d4df2466b2210596f9
Martin <matsson_755@hotmail.com>
2017-05-30 15:54:38 +0200

    errormessage på loggin

--- Decore.ClientApp/ViewModels/LoginViewModel.cs
+++ Decore.ClientApp/ViewModels/LoginViewModel.cs
@@ -1,8 +1,15 @@
-﻿namespace Decore.ClientApp.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Decore.ClientApp.ViewModels
+
 {
     public class LoginViewModel
     {
+
+       [Required(ErrorMessage = "Anget rätt användarnamn?")]
         public string Username { get; set; }
+
+       [Required(ErrorMessage = "Anget rätt lösenord?")]
         public string Password { get; set; }
     }
 }
